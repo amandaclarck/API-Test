c8c9db0bedb0147929cfcdb3f5efc9e805cd3bf1
Amanda Pedroso <amanda.lssc@gmail.com>
2020-07-24 08:44:16 -0300

    Add initialize, get_questions, get_disciplines and parameters method

--- app/adapters/q_concursos_adapter.rb
+++ app/adapters/q_concursos_adapter.rb
@@ -1,2 +1,24 @@
 class QConcursosAdapter
+  def initialize(args)
+    @repository = QConcursosRepository.new
+  end
+
+  attr_reader :repository
+
+  def self.get_questions
+    parameters("question_access.json")
+    repository(args).request
+  end
+
+  def self.get_disciplines
+    parameters("questions.json")
+    repository(args).request
+  end
+
+  private
+  def parameters(path)
+    {
+      full_url: "https://raw.githubusercontent.com/qcx/desafio-backend/master/" + path
+    }
+  end
 end
