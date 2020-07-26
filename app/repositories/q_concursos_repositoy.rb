0cd610a14b337291dab521a2090c5b73d59fad23
Amanda Pedroso <amanda.lssc@gmail.com>
2020-07-26 10:28:32 -0300

    Add QConcursosRepository class

--- app/repositories/q_concursos_repositoy.rb
+++ app/repositories/q_concursos_repositoy.rb
@@ -1,2 +1,15 @@
 class QConcursosRepository
+  def initialize(args)
+    @action = args[:action]
+    @full_url = args[:full_url]
+  end
+
+  attr_reader :action
+
+  def request
+    if action.eql? "get"
+      HTTPClient.get(url: full_url)
+    else
+    end
+  end
 end
