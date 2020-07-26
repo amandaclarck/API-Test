743c93c42b9f09a25dec60064357f590546dcda8
Amanda Pedroso <amanda.lssc@gmail.com>
2020-07-26 20:06:48 -0300

    Remove initiliaze method Improve args Add repository_request method; it instantiates repository passing the arguments and calling request method Add parameters method as self method Add action param to parameters method

--- app/adapters/q_concursos_adapter.rb
+++ app/adapters/q_concursos_adapter.rb
@@ -1,24 +1,24 @@
 class QConcursosAdapter
-  def initialize(args)
-    @repository = QConcursosRepository.new
-  end
-
-  attr_reader :repository
-
   def self.get_questions
-    parameters("question_access.json")
-    repository(args).request
+    args = parameters(action: "get", path: "question_access.json")
+    repository_request(args)
   end
 
   def self.get_disciplines
-    parameters("questions.json")
-    repository(args).request
+    args = parameters(action: "get", path: "questions.json")
+    repository_request(args)
+  end
+
+  def self.repository_request(args)
+    QConcursosRepository.new(args).request
   end
 
   private
-  def parameters(path)
+
+  def self.parameters(args)
     {
-      full_url: "https://raw.githubusercontent.com/qcx/desafio-backend/master/" + path
+      action: args[:action],
+      full_url: "https://raw.githubusercontent.com/qcx/desafio-backend/master/" + args[:path]
     }
   end
 end
