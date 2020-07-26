b09ccffad0ab8fff7c93b001c8e9ed3077ae21cc
Amanda Pedroso <amanda.lssc@gmail.com>
2020-07-26 20:05:19 -0300

    Add disciplines as class variable Add parse_disciplines method; it order by created_at desc the disciplines

--- app/services/q_concursos/disciplines_fetcher.rb
+++ app/services/q_concursos/disciplines_fetcher.rb
@@ -4,18 +4,45 @@ module QConcursos
       @adapter = QConcursosAdapter
     end
 
-    attr_reader :adapter
+    attr_reader :adapter, :disciplines
 
-    def self.perform(args)
+    def self.perform(args = {})
       new(args).perform
     end
 
     def perform
       fetch_disciplines
+      parse_disciplines
     end
 
     def fetch_disciplines
       @disciplines = adapter.get_disciplines
+    end
+
+    def parse_disciplines
+      order = "DESC"
+
+      dates = disciplines[10..20].map do |discipline|
+        {
+          "id": discipline["id"],
+          # "statement": discipline["statement"],
+          # "text": discipline["text"],
+          # "answer": discipline["answer"],
+          "daily_access": discipline["daily_access"],
+          "discipline": discipline["discipline"],
+          "created_at": discipline["created_at"]
+        }
+      end
+      dates.sort_by { |date| order == "DESC" ? -date[:created_at].to_i : date[:created_at] }
+      # d.sort! { |discipline| DateTime.parse(discipline[:created_at]) <=> DateTime.parse(discipline[:created_at]) }
+
+      # d.sort_by{ |discipline| discipline["created_at"] if !discipline["created_at"].blank? }
+
+      #parsear a resposta para hash
+      #parsear a data para datetime
+      #organizar pelas últimas 24 horas
+      #organizar pelo daily_access maior
+      #não deixar bater mais que 1 vez por minuto
     end
   end
 end
