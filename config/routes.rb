117542017f7af9ac38fe541f3be6b203ac641de4
Amanda Pedroso <amanda.lssc@gmail.com>
2020-07-29 19:30:09 -0300

    Add routes for disciplines and questions

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,4 @@
 Rails.application.routes.draw do
+  get "/disciplines/index", to: "disciplines#index", as: "disciplines"
+  post "/questions/:params_date", to: "questions#index", as: "questions"
 end
