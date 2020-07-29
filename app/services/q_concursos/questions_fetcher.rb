e8b26980f023c315459bc11d8439d7a87cd721f1
Amanda Pedroso <amanda.lssc@gmail.com>
2020-07-29 10:15:34 -0300

    Delete questions_per_year and questions_per_month methods Add params_date variable Add parsed_date method Add filtered_questions method; it receives params_date and checks if the date is a full_date, a month plus a year or just a year and then filters the result by date param

--- app/services/q_concursos/questions_fetcher.rb
+++ app/services/q_concursos/questions_fetcher.rb
@@ -2,9 +2,10 @@ module QConcursos
   class QuestionsFetcher
     def initialize(args = {})
       @adapter = QConcursosAdapter
+      @params_date = {year: "2020", month: "07", day: "13"}
     end
 
-    attr_reader :adapter, :questions, :parsed_questions
+    attr_reader :adapter, :questions, :parsed_questions, :params_date
 
     def self.perform(args = {})
       new(args).perform
@@ -13,19 +14,18 @@ module QConcursos
     def perform
       fetch_questions
       parse_questions
-      # questions_per_year(year_number)
-      # questions_per_month(month_number, year_number)
+      filtered_questions
     end
 
     def fetch_questions
-      return Rails.cache.fetch(@questions, :expires_in => 1.minutes){
+      return Rails.cache.fetch(@questions, :expires_in => 2.minutes){
         @questions = adapter.get_questions
       }
     end
 
     def parse_questions
       order = "DESC"
-
+      
       @parsed_questions = questions.map do |question|
         {
           "question_id": question["question_id"],
@@ -35,34 +35,48 @@ module QConcursos
       end.sort_by { |question| order == "DESC" ? -question[:times_accessed].to_i : question[:times_accessed] }
     end
 
-    def questions_per_week(week_number)
+    def filtered_questions
+      year = params_date[:year].to_i
+      month = params_date[:month].to_i
+      day = params_date[:day].to_i
+      is_month_is_year = is_month(month) && is_year(year)
+
+      if is_day(day) && is_month_is_year
+        full_date = Time.parse([year, month, day].join("-"))
+        start_date = full_date.beginning_of_week
+        end_date = full_date.end_of_week
+        
+        questions = parsed_questions[10..20].map do |question|
+          date = parsed_date(question[:date])
+          if (date >= start_date or date <= end_date)
+            question
+          end
+        end
+      elsif is_month_is_year
+        questions = parsed_questions[10..20].map do |question|
+          date = parsed_date(question[:date])
+          
+          if (year.eql? date.year and month.eql? date.month)
+            question
+          end
+        end
+      elsif is_year(year)
+        questions = parsed_questions[10..20].map do |question|
+          if year == parsed_date(question[:date]).year
+            question
+          end
+        end
+      else 
+        { data: "No data"} 
+      end
       
-    end
-
-    def questions_per_month(month_number, year_number)
-      if is_month(month_number) && is_year(year_number)
-        questions = parsed_questions[0..100].map do |question|
-          if (year_number.eql? DateTime.parse(question[:date]).year and
-             month_number.eql? DateTime.parse(question[:date]).month)
-             
-            question
-          end
-        end.reject(&:blank?).sort_by { |question| -question[:times_accessed]}
-      else
-        return {data: "No data"}
+      unless questions.blank?
+        questions.reject(&:blank?).sort_by { |question| -question[:times_accessed]}
       end
     end
 
-    def questions_per_year(year_number)
-      if is_year(year_number)
-        parsed_questions[10..40].map do |question| 
-          if year_number == DateTime.parse(question[:date]).year
-            question
-          end
-        end.reject(&:blank?).sort_by { |question| -question[:times_accessed]}
-      else
-        return {data: "No data"}
-      end
+    def parsed_date(date)
+      DateTime.parse(date)
     end
 
     def is_month(month)
