01e314c7cd697d1f6dfb0427b6794d2674ba0ee2
Amanda Pedroso <amanda.lssc@gmail.com>
2020-07-30 08:17:27 -0300

    Add namespace for api

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  get "/disciplines/index", to: "disciplines#index", as: "disciplines"
-  post "/questions/:params_date", to: "questions#index", as: "questions"
+  namespace :api do
+    resources :disciplines, only: [:index]
+  end
+  # get "/disciplines", to: "/api/disciplines#index", as: "disciplines"
+  # post "/questions/:params_date", to: "/api/questions#index", as: "questions"
 end
