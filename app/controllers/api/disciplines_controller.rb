07d191240e62c98688ac51a8b18f410ad4555e6a
Amanda Pedroso <amanda.lssc@gmail.com>
2020-07-30 18:49:05 -0300

    Add result as response

--- app/controllers/api/disciplines_controller.rb
+++ app/controllers/api/disciplines_controller.rb
@@ -2,8 +2,9 @@
 module Api
   class DisciplinesController < ApplicationController
     def index
-      result = QConcursos::DisciplinesFetcher.perform
-      render json: {data: "OI"}
+      @result = QConcursos::DisciplinesFetcher.perform
+      
+      render json: {data: @result }
     rescue => e
       Rails.logger.warn("#{e}: #{e.backtrace.join "\n\t"}")
     end
