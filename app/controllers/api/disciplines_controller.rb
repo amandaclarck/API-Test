c0eff726d27b05e7f5ec536827adf7a1bc85a83a
Amanda Pedroso <amanda.lssc@gmail.com>
2020-07-30 20:31:21 -0300

    Fix indentation

--- app/controllers/api/disciplines_controller.rb
+++ app/controllers/api/disciplines_controller.rb
@@ -3,8 +3,7 @@ module Api
   class DisciplinesController < ApplicationController
     def index
       @result = QConcursos::DisciplinesFetcher.perform
-      
-      render json: {data: @result }
+      render json: { data: @result }
     rescue => e
       Rails.logger.warn("#{e}: #{e.backtrace.join "\n\t"}")
     end
