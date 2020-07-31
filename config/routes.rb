e5a5795c0eaa957ee3a07a5d11775f88d6d1d5f0
Amanda Pedroso <amanda.lssc@gmail.com>
2020-07-30 21:06:13 -0300

    Add route for filtered questions

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   namespace :api do
     resources :disciplines, only: [:index]
   end
-  # get "/disciplines", to: "/api/disciplines#index", as: "disciplines"
-  # post "/questions/:params_date", to: "/api/questions#index", as: "questions"
+
+  namespace :api, defaults: {format: :json}  do
+    post "/questions/filter/" => "questions#filter"
+  end
 end
