710712e4e1c93966c0f8f3985b347da8c6c2dedb
Amanda Pedroso <amanda.lssc@gmail.com>
2020-07-30 21:07:17 -0300

    Add filter method; it gets data from QuestionsFetcher class and shows it Add date_param method; it parses to json the received date params

--- app/controllers/api/questions_controller.rb
+++ app/controllers/api/questions_controller.rb
@@ -1,5 +1,17 @@
 
   module Api
     class QuestionsController < ApplicationController
+      def filter
+        @result = QConcursos::QuestionsFetcher.perform(date_params)
+        render json: { data: @result }
+      rescue => e
+        Rails.logger.warn("#{e}: #{e.backtrace.join "\n\t"}")
+      end
+
+      private
+
+      def date_params
+        json_params = JSON.parse(request.raw_post)
+      end
     end
   end
