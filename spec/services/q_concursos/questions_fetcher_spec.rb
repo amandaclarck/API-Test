89166a41857f6e29320bb90b8e161b15c1ac70fb
Amanda Pedroso <amanda.lssc@gmail.com>
2020-08-02 17:24:42 -0300

    Add questions fetcher spec file

---
+++ spec/services/q_concursos/questions_fetcher_spec.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+describe QConcursos::QuestionsFetcher do
+  describe ".perform" do
+    it "makes a request to get questions" do
+      response_body = File.read("spec/fixtures/question_access.json")
+
+      url = "https://raw.githubusercontent.com/qcx/desafio-backend/master/question_access.json"
+      request = stub_request(:get, url)
+      .with(headers: {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 
+        'Host'=>'raw.githubusercontent.com', 'User-Agent'=>'Ruby'}).to_return(body: response_body, status: 200)
+
+      args = {}
+      described_class.perform(args)
+
+      expect(request).to have_been_requested
+    end
+  end
+end
