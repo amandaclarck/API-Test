e999c79742b58e9d53ddc64881265bd81e6b0832
Amanda Pedroso <amanda.lssc@gmail.com>
2020-08-02 20:48:48 -0300

    Remove unused methods

--- app/utils/http_client_helper.rb
+++ app/utils/http_client_helper.rb
@@ -8,44 +8,6 @@ module HttpClientHelper
     json: "application/json"
   }.freeze
 
-  def self.put(args)
-    uri = URI.parse(args[:url])
-
-    request = Net::HTTP::Put.new(uri.request_uri)
-    request.body = args[:body]
-    request["Content-Type"] = CONTENT_TYPE_HASH[args[:content_type]]
-
-    args[:headers]&.each do |header, value|
-      request[header] = value
-    end
-
-    http = Net::HTTP.new(uri.host, uri.port)
-    http.use_ssl = (uri.scheme == "https")
-    Rails.logger.info("HTTP PUT Request: #{uri} - #{request.body}")
-    response = http.request(request)
-    Rails.logger.info("HTTP PUT Response: #{response.code} - #{response.body}")
-    response
-  end
-
-  def self.post(args)
-    uri = URI.parse(args[:url])
-
-    request = Net::HTTP::Post.new(uri.path)
-    request.body = args[:body]
-    request["Content-Type"] = CONTENT_TYPE_HASH[args[:content_type]]
-
-    unless args[:headers].blank?
-      args[:headers]&.each do |header, value|
-        request[header] = value
-      end
-    end
-
-    http = Net::HTTP.new(uri.host, uri.port)
-    http.use_ssl = (uri.scheme == "https")
-
-    http.request(request)
-  end
-
   def self.get(args)
     uri = URI(args[:url])
     req = Net::HTTP::Get.new(uri)
