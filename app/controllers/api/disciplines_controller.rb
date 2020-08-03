cd2533297c14676dee94157c202f05d64aea74a9
Amanda Pedroso <amanda.lssc@gmail.com>
2020-08-02 21:32:43 -0300

    Remove status

--- app/controllers/api/disciplines_controller.rb
+++ app/controllers/api/disciplines_controller.rb
@@ -4,7 +4,7 @@ module Api
   class DisciplinesController < ApplicationController
     def index
       @result = QConcursos::DisciplinesFetcher.perform
-      render json: {data: @result, status: :ok}
+      render json: {data: @result}
     rescue => e
       Rails.logger.warn("#{e}: #{e.backtrace.join "\n\t"}")
     end
